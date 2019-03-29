a180b47ab95190f45f4fb2f7e5806fc762ecb15a
Rodrigo Muniz <muniz95@bsd.com.br>
2019-03-29 20:37:52 +0000

    Remoção de código comentado e publicação no GH Pages

--- web/main.dart
+++ web/main.dart
@@ -10,30 +10,4 @@ void main() async {
   state.counter.listen((value) => querySelector('#output').text = value.toString());
   querySelector('#increment').onClick.listen((_) => state.increment());
   querySelector('#decrement').onClick.listen((_) => state.decrement());
-  // final BehaviorSubject<int> _value = BehaviorSubject<int>();
-  
-  // _value.stream.listen((value) => print('value inserted: $value'));
-  // querySelector('body').onClick.listen((_) => _value.add(new Random().nextInt(100)));
-  // querySelector('#output').text = 'teste';
-  // const konamiKeyCodes = const <int>[
-  //   KeyCode.UP,
-  //   KeyCode.UP,
-  //   KeyCode.DOWN,
-  //   KeyCode.DOWN,
-  //   KeyCode.LEFT,
-  //   KeyCode.RIGHT,
-  //   KeyCode.LEFT,
-  //   KeyCode.RIGHT,
-  //   KeyCode.B,
-  //   KeyCode.A];
-
-  // final result = querySelector('#output');
-  // final keyUp = new Observable<KeyboardEvent>(document.onKeyUp);
-
-  // keyUp
-  //   .map((event) => event.keyCode)
-  //   .bufferCount(10, 1)
-  //   .where((lastTenKeyCodes) => const ListEquality<int>().equals(lastTenKeyCodes, konamiKeyCodes))
-  //   .listen((_) => result.innerHtml = 'KONAMI!');
-  
 }
