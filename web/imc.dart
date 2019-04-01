579791abcf084406add4a401952d6f7a9ad3b103
Rodrigo Muniz <muniz95@bsd.com.br>
2019-04-01 18:23:32 +0000

    Conteúdo removido do main.dart

--- web/imc.dart
+++ web/imc.dart
@@ -5,8 +5,6 @@ void main() async {
   var state = new IMC()..init();
   Function height = (KeyboardEvent e) => state.setHeight((e.target as NumberInputElement).valueAsNumber);
   Function weight = (KeyboardEvent e) => state.setWeight((e.target as NumberInputElement).valueAsNumber);
-  // state.height.listen((value) => querySelector('#streamHeight').text = value.toString());
-  // state.weight.listen((value) => querySelector('#streamWeight').text = value.toString());
   state.imc.listen((value) => querySelector('#imc').text = value.toString());
   querySelector('#height').onKeyUp.listen(height);
   querySelector('#weight').onKeyUp.listen(weight);
--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,2 @@
-import 'dart:html';
-import 'package:dart_pwa_sb/counter_bloc.dart';
-
-void main() async {
-  var state = new Counter()..init();
-  Function increment = (MouseEvent e) => state.increment();
-  Function decrement = (MouseEvent e) => state.decrement();
-  state.counter.listen((value) => querySelector('#output').text = value.toString());
-  querySelector('#increment').onClick.listen(increment);
-  querySelector('#decrement').onClick.listen(decrement);
+void main() {
 }
