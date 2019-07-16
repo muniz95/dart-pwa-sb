c29838dfd58697e6365ac57877ee620e65273a20
Rodrigo Muniz <rtmuniz@positivo.com.br>
2019-07-16 17:52:42 -0300

    Upgraded pwa lib

--- web/counter.dart
+++ web/counter.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 import 'package:dart_pwa_sb/counter_bloc.dart';
 
 void main() async {
-  var state = new Counter()..init();
+  var state = Counter()..init();
   Function increment = (MouseEvent e) => state.increment();
   Function decrement = (MouseEvent e) => state.decrement();
   state.counter.listen((value) => querySelector('#output').text = value.toString());
--- web/imc.dart
+++ web/imc.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 import 'package:dart_pwa_sb/imc_bloc.dart';
 
 void main() async {
-  var state = new IMC()..init();
+  var state = IMC()..init();
   Function height = (KeyboardEvent e) => state.setHeight((e.target as NumberInputElement).valueAsNumber);
   Function weight = (KeyboardEvent e) => state.setWeight((e.target as NumberInputElement).valueAsNumber);
   state.imc.listen((value) => querySelector('#imc').text = value.toString());
